e0c5823da7f1a94219d01fcdb0de31837b524fc2
Ксения <korzhuk.xeniya@yandex.ru>
2023-05-16 22:17:25 +0300

    task-7

--- src/Main.java
+++ src/Main.java
@@ -2,37 +2,65 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        task1 ();
-        task2 ();
-        task3 ();
-        task4 ();
-        task5 ();
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
     }
-    public static void task1 () {
+
+    public static void task1() {
         System.out.println("Задача 1");
         for (int i = 1; i <= 10; i = i + 1) {
             System.out.println(i);
         }
     }
-    public static void task2 () {
+
+    public static void task2() {
         System.out.println("Задача 2");
         for (int i = 10; i > 0; i = i - 1) {
             System.out.println(i);
         }
     }
-    public static void task3 () {
+
+    public static void task3() {
         System.out.println("Задача 3");
         for (int i = 0; i < 17; i = i + 2) {
             System.out.println(i);
         }
     }
-    public static void task4 () {
+
+    public static void task4() {
         System.out.println("Задача 4");
         for (int i = 10; i > -11; i = i - 1) {
             System.out.println(i);
         }
     }
-    public static void task5 () {
+
+    public static void task5() {
         System.out.println("Задача 5");
+        for (int i = 1904; i < 2097; i = i + 4) {
+            System.out.println(i + " год является високосным");
+        }
+    }
+
+    public static void task6() {
+        System.out.println("Задача 6");
+        for (int i = 7; i < 99; i = i + 7) {
+            System.out.print(i + " ");
+
+        }
+        System.out.println(" ");
+    }
+
+    public static void task7() {
+        System.out.println("Задача 7");
+        for (int i = 1; i < 513; i = i * 2) {
+            System.out.print(i + " ");
+
+        }
+        System.out.println(" ");
     }
 }
